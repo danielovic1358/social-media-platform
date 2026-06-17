c760b96eeded208a68ba9ca2563b5d70b50d11c8
danielovic1358 <danielstroh283@gmail.com>
2026-06-17 15:01:33 +0200

    Änderungen vom 17.06

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
-<script setup lang="ts">
+ <script setup lang="ts">
+import axios from 'axios'
 import "./assets/main.css";
+import { onBeforeMount, onMounted, ref } from 'vue';
 import Friends from "./Friends.vue";
+import Messages from "./Message.vue";
 let keyword = "";
 const routes = {
     "/friends": Friends,
+    "/messages": Messages,
 }
+const getPost = ref(<any>{})
+
+async function loadPost() {
+  const postFromServer = await axios.get("http://localhost:3000/posts")
+  getPost.value = postFromServer.data[0]
+  console.log(getPost.value.imageURL)
+}
+
+onBeforeMount(async()=>{
+  await loadPost()
+})
 </script>
 
 <template>
   <ul class="topnav">
-    <a href="http://localhost:5173/">
+    <a href="http://localhost:5173/start">
       <img class="icons" src="./assets/home.png" alt="Homebutton">
     </a>
     <a href="http://localhost:5173/friends">
       <img class="icons" src="./assets/friends.png" alt="Friendlist">
     </a>
-    <a href="http://localhost:5173/">
+    <a href="http://localhost:5173/messages">
       <img class="icons" src="./assets/messages.png" alt="Messages">
     </a>
     <input v-model="keyword" type="text">
@@ -29,7 +44,7 @@ const routes = {
       <img class="profiles" src="./assets/account1.png">
     </div>
     <div>
-      <img class="posts" src="./assets/test1.jpg">
+      <img class="posts" :src="getPost.imageURL" alt="Beitrag">
       <span class="arrowkeys">
         <button onclick="alert('Letzter Beitrag.')"><img class="arrowkeys" src="./assets/arrowup.png"></button>
         <button onclick="alert('Nächster Beitrag.')"><img class="arrowkeys" src="./assets/arrowdown.png"></button>
